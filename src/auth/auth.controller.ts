0dd17f00dd0e07b875418294b35cac191d7ea185
RamKumar-Muthuraj <muthuraj.ramkumar@gwcdata.ai>
2026-07-03 18:30:18 +0530

    Google Authentication callback update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,42 +1,36 @@
-import { Controller, Get, Req, Res, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport/dist/auth.guard";
-import { AuthService } from "./auth.service";
-
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport/dist/auth.guard';
+import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
-    constructor(private authService: AuthService) {}
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  googleAuth() {}
 
-@Get('google')
-@UseGuards(AuthGuard('google'))
-googleAuth() {}
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleCallback(@Req() req, @Res() res) {
+    const result = await this.authService.googleLogin(req.user);
 
-
-@Get('google/callback')
-@UseGuards(AuthGuard('google'))
-async googleCallback(
-  @Req() req,
-  @Res() res,
-) {
-  const result =
-    await this.authService.googleLogin(
-      req.user,
-    );
-
-  res.cookie(
-    'access_token',
-    result.token,
-    {
+    res.cookie('access_token', result.token, {
       httpOnly: true,
-      secure:
-        process.env.NODE_ENV ===
-        'production',
-      sameSite: 'lax',
-    },
-  );
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
+    });
+    console.log(
+  "Redirecting to:",
+  `${this.configService.get("FRONTEND_URL")}/Home`
+);
+    const frontendUrl =
+      this.configService.get<string>('FRONTEND_URL') ?? 'http://localhost:3000';
 
-  return res.redirect("/Home");
-}
-
+    return res.redirect(`${frontendUrl}/Home`);
+  }
 }
